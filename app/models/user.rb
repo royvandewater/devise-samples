c813d57cdd8d29fa38876ca09c85a4be7f4665a6
Integrum P S 12 <support@integrumtech.com>
2011-01-25 17:53:41 -0700

    Allow users to login with either email or login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
+  protected
+  def self.find_for_database_authentication(conditions)
+    login = conditions.delete(:login)
+    where(conditions).where(["username = :login OR email = :login", {:login => login}]).first
+  end
 end
